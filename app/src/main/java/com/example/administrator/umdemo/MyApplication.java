3530d1b2853c92eebfd760ee1f0cff6e79fee35a
speak-out <lj_longjian@sian.cn>
2016-05-17 22:28:44 +0800

    第三方分享

---
+++ app/src/main/java/com/example/administrator/umdemo/MyApplication.java
@@ -0,0 +1,22 @@
+package com.example.administrator.umdemo;
+
+import android.app.Application;
+
+import com.umeng.socialize.PlatformConfig;
+
+/**
+ * Created by Administrator on 2016/5/17 0017.
+ */
+public class MyApplication extends Application {
+
+//    PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+//    //微信 appid appsecret
+//    PlatformConfig.setSinaWeibo("3921700954","04b48b094faeb16683c32669824ebdad");
+//    //新浪微博 appkey appsecret     PlatformConfig.setYixin("yxc0614e80c9304c11b0391514d09f13bf");
+//    //易信 appkey        PlatformConfig.setRenren("201874","28401c0964f04a72a14c812d6132fcef","3bf66e42db1e4fa9829b955cc300b737");
+//    //人人 appid appkey appsecret
+//    PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+//    // qq qzone appid appkey
+//    PlatformConfig.setAlipay("2015111700822536");
+//    //alipay appid
+}
